c880be44efefddb80fd2bb71fb19cbf622ac43c3
yaroslavsf <yaroslav1598753@gmail.com>
2023-02-24 10:13:30 +0100

    Initial commit with db config

---
+++ src/test/java/ch/noseryoung/realestate/RealestateApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
